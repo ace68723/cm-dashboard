4f4d3c37fa9afaac6263ec3fbc4ca678c8d7c8ef
ace68723 <ace68723@gmail.com>
2017-01-20 15:37:38 -0500

    feat(DriverTimeModule):add angular version

---
+++ www/DriverTimeModule/angular_version/www/js/main.js
@@ -0,0 +1,155 @@
+/***
+Metronic AngularJS App Main Script
+***/
+
+/* Metronic App */
+var MetronicApp = angular.module("MetronicApp", [
+    "ui.router",
+    "ui.bootstrap",
+    "oc.lazyLoad",
+    "ngSanitize"
+]);
+
+/* Configure ocLazyLoader(refer: https://github.com/ocombe/ocLazyLoad) */
+MetronicApp.config(['$ocLazyLoadProvider', function($ocLazyLoadProvider) {
+    $ocLazyLoadProvider.config({
+        // global configs go here
+    });
+}]);
+
+/********************************************
+ BEGIN: BREAKING CHANGE in AngularJS v1.3.x:
+*********************************************/
+/**
+`$controller` will no longer look for controllers on `window`.
+The old behavior of looking on `window` for controllers was originally intended
+for use in examples, demos, and toy apps. We found that allowing global controller
+functions encouraged poor practices, so we resolved to disable this behavior by
+default.
+
+To migrate, register your controllers with modules rather than exposing them
+as globals:
+
+Before:
+
+```javascript
+function MyController() {
+  // ...
+}
+```
+
+After:
+
+```javascript
+angular.module('myApp', []).controller('MyController', [function() {
+  // ...
+}]);
+
+Although it's not recommended, you can re-enable the old behavior like this:
+
+```javascript
+angular.module('myModule').config(['$controllerProvider', function($controllerProvider) {
+  // this option might be handy for migrating old apps, but please don't use it
+  // in new ones!
+  $controllerProvider.allowGlobals();
+}]);
+**/
+
+//AngularJS v1.3.x workaround for old style controller declarition in HTML
+MetronicApp.config(['$controllerProvider', function($controllerProvider) {
+  // this option might be handy for migrating old apps, but please don't use it
+  // in new ones!
+  $controllerProvider.allowGlobals();
+}]);
+
+/********************************************
+ END: BREAKING CHANGE in AngularJS v1.3.x:
+*********************************************/
+
+/* Setup global settings */
+MetronicApp.factory('settings', ['$rootScope', function($rootScope) {
+    // supported languages
+    var settings = {
+        layout: {
+            pageSidebarClosed: false, // sidebar menu state
+            pageContentWhite: true, // set page content layout
+            pageBodySolid: false, // solid body color state
+            pageAutoScrollOnLoad: 1000 // auto scroll to top on page load
+        },
+        assetsPath: '../assets',
+        globalPath: '../assets/global',
+        layoutPath: '../assets/layouts/layout4',
+    };
+
+    $rootScope.settings = settings;
+
+    return settings;
+}]);
+
+/* Setup App Main Controller */
+MetronicApp.controller('AppController', ['$scope', '$rootScope', function($scope, $rootScope) {
+    $scope.$on('$viewContentLoaded', function() {
+        App.initComponents(); // init core components
+        //Layout.init(); //  Init entire layout(header, footer, sidebar, etc) on page load if the partials included in server side instead of loading with ng-include directive
+    });
+}]);
+
+/***
+Layout Partials.
+By default the partials are loaded through AngularJS ng-include directive. In case they loaded in server side(e.g: PHP include function) then below partial
+initialization can be disabled and Layout.init() should be called on page load complete as explained above.
+***/
+
+
+
+
+
+/* Setup Layout Part - Sidebar */
+MetronicApp.controller('PageHeadController', ['$scope', function($scope) {
+    $scope.$on('$includeContentLoaded', function() {
+        Demo.init(); // init theme panel
+    });
+}]);
+
+
+
+
+
+/* Setup Rounting For All Pages */
+MetronicApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    // Redirect any unmatched url
+    $urlRouterProvider.otherwise("/pickers.html");
+
+    $stateProvider
+
+        // Date & Time Pickers
+        .state('pickers', {
+            url: "/pickers",
+            templateUrl: "views/pickers.html",
+            data: {pageTitle: 'Date & Time Pickers'},
+            controller: "GeneralPageController",
+            resolve: {
+                deps: ['$ocLazyLoad', function($ocLazyLoad) {
+                    return $ocLazyLoad.load([{
+                        name: 'MetronicApp',
+                        insertBefore: '#ng_load_plugins_before', // load the above css files before '#ng_load_plugins_before'
+                        files: [
+                            '../assets/global/plugins/bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css',
+                            '../assets/global/plugins/bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js',
+                            '../assets/pages/scripts/components-date-time-pickers.min.js',
+
+                        ]
+                    }]);
+                }]
+            }
+        })
+
+
+
+}]);
+
+/* Init global settings and run the app */
+MetronicApp.run(["$rootScope", "settings", "$state", function($rootScope, settings, $state) {
+    $rootScope.$state = $state; // state to be accessed from view
+    $rootScope.$settings = settings; // state to be accessed from view
+}]);
